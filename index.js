3746fd74f58cad16b8c256235bbc784c7bc76e39
sohivot <151276108+sohivot@users.noreply.github.com>
2025-12-12 03:53:06 +0700

    Change login credentials in index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function validateForm() {
     var password = document.getElementById("password").value;
 
     //validasi username dan password, username dan password bisa diganti sesuai keinginan
-    if (username === "pacar kalian" && password === "pacar kalian") {
+    if (username === "ciwi" && password === "ciwi") {
         Swal.fire({
             icon: "success",
             title: "Login berhasil!",
